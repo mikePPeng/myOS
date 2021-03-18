52b0c72d9eefc271bd7c4375567140956d5d5820
mikePPeng <285599876@qq.com>
2021-03-18 20:05:58 +0800

    delete build results

--- Debug/Core/Src/kernel_src/interrupt.d
+++
@@ -1,4 +0,0 @@
-Core/Src/kernel_src/interrupt.o: ../Core/Src/kernel_src/interrupt.c \
- ../Core/Inc/kernel_inc/interrupt.h
-
-../Core/Inc/kernel_inc/interrupt.h:
--- Debug/Core/Src/kernel_src/soft_timer.d
+++
@@ -1,6 +0,0 @@
-Core/Src/kernel_src/soft_timer.o: ../Core/Src/kernel_src/soft_timer.c \
- ../Core/Inc/kernel_inc/soft_timer.h ../Core/Inc/kernel_inc/common.h
-
-../Core/Inc/kernel_inc/soft_timer.h:
-
-../Core/Inc/kernel_inc/common.h:
